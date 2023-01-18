bc6d8806683cc79e071bbb39b3db4ad7a9d25520
Yuliia-Harkusha <97845497+Yuliia-Harkusha@users.noreply.github.com>
2023-01-18 19:30:40 +0100

    basename

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
-import { AppProvider } from "./context";
-import { theme } from "./theme";
-import "./index.css";
-import { App } from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { AppProvider } from './context';
+import { theme } from './theme';
+import './index.css';
+import { App } from './App';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
     <AppProvider>
       <ThemeProvider theme={theme}>
-        <BrowserRouter>
+        <BrowserRouter basename="cocktails-project">
           <App />
         </BrowserRouter>
       </ThemeProvider>
