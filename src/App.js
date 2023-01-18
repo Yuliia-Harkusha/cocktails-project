5c4b836c288c9b3f4467ddc4072ccb70e4a9760f
Yuliia-Harkusha <97845497+Yuliia-Harkusha@users.noreply.github.com>
2023-01-18 21:03:01 +0100

    Home page import

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React from "react";
-import { lazy, Suspense } from "react";
-import { Route, Routes } from "react-router-dom";
-import { Navbar } from "./components/Navbar/Navbar";
-import { Loader } from "./components/Loader/Loader";
+import React from 'react';
+import { lazy, Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Navbar } from './components/Navbar/Navbar';
+import { Loader } from './components/Loader/Loader';
 
-const HomePage = lazy(() => import("./pages/Home/Home"));
+import Home from './pages/Home/Home';
+
+// const HomePage = lazy(() => import("./pages/Home/Home"));
 const SingleCocktailPage = lazy(() =>
-  import("./pages/SingleCocktail/SingleCocktail")
+  import('./pages/SingleCocktail/SingleCocktail')
 );
-const AboutPage = lazy(() => import("./pages/About/About"));
-const ErrorPage = lazy(() => import("./pages/Error/Error"));
+const AboutPage = lazy(() => import('./pages/About/About'));
+const ErrorPage = lazy(() => import('./pages/Error/Error'));
 
 export const App = () => {
   return (
@@ -18,7 +20,7 @@ export const App = () => {
 
       <Suspense fallback={<Loader />}>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<Home />} />
           <Route path="about" element={<AboutPage />} />
           <Route path="cocktail/:id" element={<SingleCocktailPage />} />
           <Route path="*" element={<ErrorPage />} />
